6f648c2cbb65b7fbe00078a3b184369c4c6bf312
vrammun685 <victtorramirezz1@gmail.com>
2024-11-13 12:56:51 +0100

    si

--- Alumno/Clase.py
+++ Alumno/Clase.py
@@ -19,3 +19,21 @@ for num in list:
         nlista.append(num)
 
 print(nlista)
+
+print(sum(num for num in list if num>10))
+
+
+
+print(min([num for num in list if num >10 ]))
+
+
+lista2=[{"nombre":"si","edad":34},{"nombre":"no","edad":20},{"nombre":"po","edad":10}]
+for persona in lista2:
+    for atributo in persona:
+        if (atributo=="edad"):
+            print(persona[atributo])
+
+print(sum(persona["edad"] for persona in lista2)/len(lista2))
+
+ListaMayore=[persona["edad"] for persona in lista2 if persona["edad"] >= 18]
+print(sum(ListaMayore)/len(ListaMayore))
